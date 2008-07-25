bf69aac52156864611e0b8329cb7b9d8600d1f54
james.strachan <james.strachan@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-25 10:42:21 +0000

    moved the code to camel 1.4.0, added a dependency on camel-spring as well and added a refactored SpringRouteContentProvider for working with Spring based routes

--- src/com/googlecode/camelrouteviewer/content/RouteContentProvider.java
+++ src/com/googlecode/camelrouteviewer/content/RouteContentProvider.java
@@ -7,24 +7,17 @@ import java.util.List;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.management.JmxSystemPropertyKeys;
-import org.apache.camel.model.ChoiceType;
 import org.apache.camel.model.FromType;
-import org.apache.camel.model.MulticastType;
 import org.apache.camel.model.ProcessorType;
 import org.apache.camel.model.RouteType;
-import org.apache.camel.util.UuidGenerator;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.zest.core.viewers.IGraphContentProvider;
-
-import com.googlecode.camelrouteviewer.views.RouteSource;
 
 import bsh.EvalError;
 import bsh.Interpreter;
 
-public class RouteContentProvider implements IGraphContentProvider {
+import com.googlecode.camelrouteviewer.views.RouteSource;
 
-	private RoutesStore routesTempStore;
+public class RouteContentProvider extends RouteContentProviderSupport {
 
 	private CamelContext context;
 
@@ -35,6 +28,26 @@ public class RouteContentProvider implements IGraphContentProvider {
 		context = new DefaultCamelContext();
 
 		interpreter = new Interpreter();
+	}
+
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if (newInput instanceof RouteSource) {
+			routesTempStore.clear();
+			try {
+				context.stop();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			context = new DefaultCamelContext();
+			RouteSource routeSource = (RouteSource) newInput;
+
+			String beanShellScript = routeSource.getBeanShellScript();
+			buildRoute(context, beanShellScript);
+
+			viewer.refresh();
+		}
 	}
 
 	private void buildRoute(CamelContext context, String routeBuilderBSH) {
@@ -60,89 +73,6 @@ public class RouteContentProvider implements IGraphContentProvider {
 
 		List<RouteType> routes = context.getRouteDefinitions();
 
-		for (RouteType route : routes) {
-
-			FromType fromType = route.getInputs().get(0);
-
-			RouteNode fromNodeData = routesTempStore.getNodeData(fromType);
-			for (ProcessorType output : route.getOutputs()) {
-				RouteNode newData = getNodeData(routesTempStore, fromNodeData,
-						output);
-
-				fromNodeData = newData;
-
-			}
-
-		}
-	}
-
-	public RouteNode getNodeData(RoutesStore routesTempStore, RouteNode from,
-			ProcessorType processorType) {
-		RouteNode processorNodeData = routesTempStore.getNodeData(processorType);
-
-		// ^_^ pls let me use camel utils
-		UuidGenerator uuidGenerator = new UuidGenerator("routeview");
-		String connectionString = uuidGenerator.generateSanitizedId();
-		RouteConnection routeConnection = new RouteConnection(from,
-				connectionString, processorNodeData);
-		routesTempStore.getConnections().add(routeConnection);
-
-		List<ProcessorType> outputs = processorNodeData.outputs;
-
-		if (outputs != null) {
-			for (ProcessorType output : outputs) {
-				RouteNode newNodeData = getNodeData(routesTempStore,
-						processorNodeData, output);
-				if (!isMulticastNode(processorType)) {
-					processorNodeData = newNodeData;
-				}
-			}
-		}
-
-		return processorNodeData;
-	}
-
-	protected boolean isMulticastNode(ProcessorType node) {
-		return node instanceof MulticastType || node instanceof ChoiceType;
-	}
-
-	public Object getSource(Object rel) {
-		RouteConnection compareConnection = (RouteConnection) rel;
-		return routesTempStore.getSource(compareConnection);
-	}
-
-	public Object[] getElements(Object input) {
-		return routesTempStore.getConnectionsAsArray();
-	}
-
-	public Object getDestination(Object rel) {
-		RouteConnection compareConnection = (RouteConnection) rel;
-		return routesTempStore.getDestination(compareConnection);
-	}
-
-	public double getWeight(Object connection) {
-		return 0;
-	}
-
-	public void dispose() {
-	}
-
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		if (newInput instanceof RouteSource) {
-			routesTempStore.clear();
-			try {
-				context.stop();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			context = new DefaultCamelContext();
-			RouteSource routeSource = (RouteSource) newInput;
-
-			String beanShellScript = routeSource.getBeanShellScript();
-			buildRoute(context, beanShellScript);
-
-			viewer.refresh();
-		}
+		buildRoute(routes);
 	}
 }
