62432f0a84bf67b0c6dedad08f65a740deac0ee4
jmszhang <jmszhang@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-26 10:08:37 +0000

    initial version.can get InpuStream from navigator.

--- trunk/src/com/googlecode/camelrouteviewer/action/SpringVisualizeAction.java
+++ trunk/src/com/googlecode/camelrouteviewer/action/SpringVisualizeAction.java
@@ -1,12 +1,24 @@
 package com.googlecode.camelrouteviewer.action;
 
+import java.io.File;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.AbstractRulerActionDelegate;
 
+import com.googlecode.camelrouteviewer.views.RouteView;
+
+//TODO move to run as menu?
 public class SpringVisualizeAction implements IObjectActionDelegate {
 	ISelection selection;
 
@@ -16,14 +28,38 @@ public class SpringVisualizeAction implements IObjectActionDelegate {
 	}
 
 	public void run(IAction arg0) {
-		if (!(selection instanceof IStructuredSelection))
+		if (!(selection instanceof TreeSelection)){
 			return;
-		IStructuredSelection structured = (IStructuredSelection) selection;
-		IType type = (IType) structured.getFirstElement();
+		}
 
+		TreeSelection treeSelection = (TreeSelection) selection;
+		Object element = treeSelection.getFirstElement();
+		System.out.println(element.getClass());
+		
+		if(element instanceof IFile){
+			IFile file = (IFile)element;
+			InputStream inputStream = null;
+			try {
+				inputStream = file.getContents();
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			not yet work now			
+//			try{
+//				RouteView  routeView   =   (RouteView)PlatformUI.getWorkbench().
+//			        getActiveWorkbenchWindow().getActivePage().findView(RouteView.VIEW_ID); 
+//				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().bringToTop(routeView); 
+//			}catch(Exception exception){
+//				exception.printStackTrace();
+//			}
+			
+			//routeView.getGraphViewer().setInput(inputStream);
+			
+		}
 	}
 
-	public void selectionChanged(IAction arg0, ISelection arg1) {
+	public void selectionChanged(IAction arg0, ISelection selection) {
 		this.selection = selection;
 
 	}
