52367986b533d2dc8da5dc23b660871ec643c27a
jmszhang <jmszhang@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-28 15:53:20 +0000

    "run configurations.." begins works now~.And pls right click the "camel spring xml dsl " in the navigator, you will find the "run as"..

---
+++ trunk/src/com/googlecode/camelrouteviewer/launcher/CamelLaunchShortcut.java
@@ -0,0 +1,19 @@
+package com.googlecode.camelrouteviewer.launcher;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class CamelLaunchShortcut implements ILaunchShortcut{
+
+	public void launch(ISelection arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void launch(IEditorPart arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
