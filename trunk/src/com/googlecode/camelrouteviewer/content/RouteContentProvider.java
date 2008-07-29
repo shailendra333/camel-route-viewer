10f9d582dc8c3629c31c61b6d2d534b47e715553
james.strachan <james.strachan@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-29 08:47:16 +0000

    refactor to simplify the content provider code considerably

--- trunk/src/com/googlecode/camelrouteviewer/content/RouteContentProvider.java
+++ trunk/src/com/googlecode/camelrouteviewer/content/RouteContentProvider.java
@@ -1,77 +1,119 @@
 package com.googlecode.camelrouteviewer.content;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.List;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.ChoiceType;
 import org.apache.camel.model.FromType;
+import org.apache.camel.model.MulticastType;
 import org.apache.camel.model.ProcessorType;
 import org.apache.camel.model.RouteType;
 import org.eclipse.jface.viewers.Viewer;
-
-import bsh.EvalError;
-import bsh.Interpreter;
+import org.eclipse.zest.core.viewers.IGraphContentProvider;
 
 import com.googlecode.camelrouteviewer.views.RouteSource;
 
-public class RouteContentProvider extends RouteContentProviderSupport {
+/**
+ * Can handle either a {@link List<RouteType>} or a {@link RouteSource} as the input
+ * which is then converted into the content for the viewer
+ */
+public class RouteContentProvider implements IGraphContentProvider {
 
-	private CamelContext context;
-
-	private Interpreter interpreter;
+	private RouteSourceToDefinitionConverter converter = new RouteSourceToDefinitionConverter();
+	protected RoutesStore routesTempStore = new RoutesStore();
 
 	public RouteContentProvider() {
-		context = new DefaultCamelContext();
-
-		interpreter = new Interpreter();
 	}
-
-
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		if (newInput instanceof RouteSource) {
-			routesTempStore.clear();
-			try {
-				context.stop();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			context = new DefaultCamelContext();
-			RouteSource routeSource = (RouteSource) newInput;
-
-			String beanShellScript = routeSource.getBeanShellScript();
-			buildRoute(context, beanShellScript);
-
-			viewer.refresh();
-		}
-	}
-
-	private void buildRoute(CamelContext context, String routeBuilderBSH) {
-		BufferedReader bufferedreader = null;
-
+	
+	public void setRouteDefinitions(List<RouteType> routes) {
+			clear();
 		try {
-			bufferedreader = new BufferedReader(new StringReader(
-					routeBuilderBSH));
-			interpreter.set("context", context);
-			interpreter.eval(bufferedreader);
-		} catch (EvalError e) {
-			// FOR DEBUG
+			buildRoute(routes);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return;
-		} finally {
-			try {
-				bufferedreader.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if (newInput instanceof List) {
+			List<RouteType> routes = (List<RouteType>) newInput;
+			setRouteDefinitions(routes);
+		}
+		else if (newInput instanceof RouteSource) {
+			List<RouteType> routes = converter.convert((RouteSource) newInput);
+			setRouteDefinitions(routes);
+		}
+	}
+
+	public RouteNode getNodeData(RoutesStore routesTempStore, RouteNode from, ProcessorType processorType) {
+		RouteNode processorNodeData = routesTempStore.getNodeData(processorType);
+	
+		//ensure:connectionString is unique in RoutesStore.
+		String connectionString = from.id+"->"+processorType.idOrCreate();
+		RouteConnection routeConnection = new RouteConnection(from,
+				connectionString, processorNodeData);
+		routesTempStore.getConnections().add(routeConnection);
+	
+		List<ProcessorType> outputs = processorNodeData.outputs;
+	
+		if (outputs != null) {
+			for (ProcessorType output : outputs) {
+				RouteNode newNodeData = getNodeData(routesTempStore,
+						processorNodeData, output);
+				if (!isMulticastNode(processorType)) {
+					processorNodeData = newNodeData;
+				}
 			}
 		}
+	
+		return processorNodeData;
+	}
 
-		List<RouteType> routes = context.getRouteDefinitions();
+	protected boolean isMulticastNode(ProcessorType node) {
+		return node instanceof MulticastType || node instanceof ChoiceType;
+	}
 
-		buildRoute(routes);
+	public Object getSource(Object rel) {
+		RouteConnection compareConnection = (RouteConnection) rel;
+		return routesTempStore.getSource(compareConnection);
+	}
+
+	public Object[] getElements(Object input) {
+		return routesTempStore.getConnectionsAsArray();
+	}
+
+	public Object getDestination(Object rel) {
+		RouteConnection compareConnection = (RouteConnection) rel;
+		return routesTempStore.getDestination(compareConnection);
+	}
+
+	public double getWeight(Object connection) {
+		return 0;
+	}
+
+	public void dispose() {
+	}
+
+	protected void buildRoute(List<RouteType> routes) {
+	
+		for (RouteType route : routes) {
+	
+			FromType fromType = route.getInputs().get(0);
+	
+			RouteNode fromNodeData = routesTempStore.getNodeData(fromType);
+			for (ProcessorType output : route.getOutputs()) {
+				RouteNode newData = getNodeData(routesTempStore, fromNodeData,
+						output);
+	
+				fromNodeData = newData;
+	
+			}
+	
+		}
+	}
+
+	public void clear() {
+		routesTempStore.clear();
 	}
 }
