7bbb104dc177f1d144019c3e9ba166f53b534c26
james.strachan <james.strachan@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-29 13:21:07 +0000

    added fix for http://code.google.com/p/camel-route-viewer/issues/detail?id=7

--- src/com/googlecode/camelrouteviewer/launcher/CamelLaunchShortcut.java
+++ src/com/googlecode/camelrouteviewer/launcher/CamelLaunchShortcut.java
@@ -1,14 +1,26 @@
 package com.googlecode.camelrouteviewer.launcher;
 
+import java.util.Iterator;
+
 import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorPart;
 
 public class CamelLaunchShortcut implements ILaunchShortcut{
 
-	public void launch(ISelection arg0, String arg1) {
+	public void launch(ISelection selection, String mode) {
 		// TODO Auto-generated method stub
-		System.out.println("Launching: " + arg0 + " with " + arg1);
+		System.out.println("Launching: " + selection + " with " + mode);
+		if (selection instanceof TreeSelection) {
+			TreeSelection treeSelection = (TreeSelection) selection;
+			Iterator iterator = treeSelection.iterator();
+			while (iterator.hasNext()) {
+				Object value = iterator.next();
+				System.out.println("Found: " + value + " of type: " + value.getClass());
+			}
+			
+		}
 	}
 
 	public void launch(IEditorPart arg0, String arg1) {
