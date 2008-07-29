b25de9d66aeed93aeef627a53654c84881328dbd
james.strachan <james.strachan@0086039c-1f52-0410-9f54-838c103a7f1b>
2008-07-29 11:42:51 +0000

    improved some of the launching code - we now include the JRE tabs when launching camel - so folks can customize the classpath / JVM arguments and so forth. as a hack we hard code some arguments when running the delegate; but it'd be better to auto-default them - see http://code.google.com/p/camel-route-viewer/issues/detail?id=4

--- trunk/src/com/googlecode/camelrouteviewer/launcher/CamelLaunchShortcut.java
+++ trunk/src/com/googlecode/camelrouteviewer/launcher/CamelLaunchShortcut.java
@@ -8,7 +8,7 @@ public class CamelLaunchShortcut implements ILaunchShortcut{
 
 	public void launch(ISelection arg0, String arg1) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Launching: " + arg0 + " with " + arg1);
 	}
 
 	public void launch(IEditorPart arg0, String arg1) {
